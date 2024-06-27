cfe7a8f7998a49c13f77c0a13c8e2858c6267463
Keerthana <surji73@gmail.com>
2024-06-27 08:18:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function scrollToSection(aboutme) {
+    document.getElementById(aboutme).scrollIntoView({ behavior: 'smooth' });
+}
+
+function openResume() {
+    window.open('https://drive.google.com/file/d/1X0dUcNLj8QJre6ywtIQRw5-GiveB41AX/view', '_blank');
+}
+
+function scrollToSection(footer) {
+    document.getElementById(footer).scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function openGithub() {
+    window.open('https://github.com/content-123', '_blank');
+}
+
+function openLinkedin(){
+    window.open('https://www.linkedin.com/in/keerthana-kumar-1ba02585/','_blank');
+}
+
+function scrollToSection(name){
+    document.getElementById(name).scrollIntoView({ behavior: 'smooth' });
+}
